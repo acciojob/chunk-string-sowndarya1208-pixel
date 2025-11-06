dd59c171b0ea9a30838356b6c9dbc130228e09be
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-06 18:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function stringChop(str, size) {
   // your code here
-}
+
+    if (size <= 0 || str.length <= size) {
+        return [str];
+    }
+    
+    const chunks = [];
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.substring(i, i + size));
+    }
+    return chunks;
+	}
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
